ebf00831deff50f4e9cbb75c3847a476992eaeda
JeremyGamer13 <69337718+JeremyGamer13@users.noreply.github.com>
2023-09-10 05:10:39 -0600

    gs2ml installer nearly done

--- src/routes/launcher/+page.svelte
+++ src/routes/launcher/+page.svelte
@@ -2,6 +2,9 @@
     import * as fs from "@tauri-apps/api/fs";
     import * as path from "@tauri-apps/api/path";
     import * as os from "@tauri-apps/api/os";
+    import * as shell from "@tauri-apps/api/shell";
+    import * as http from "@tauri-apps/api/http";
+    // http does not use CORS
 
     import NavigationBar from "$lib/components/NavigationBar/Bar.svelte";
     import SaveSlot from "$lib/components/SaveSlot/Button.svelte";
@@ -21,6 +24,9 @@
         x86: "https://download.visualstudio.microsoft.com/download/pr/fbf3c93d-757e-46fe-ad9c-90105d48a3e8/0fd732e33dd3f18e2c5c269bfd94e505/dotnet-sdk-6.0.413-win-x86.exe",
         arm: "https://download.visualstudio.microsoft.com/download/pr/65921ee8-7be8-42f7-8902-900c98acd5e0/818423a2eb96f680225390e706e9994e/dotnet-sdk-6.0.413-win-arm64.exe",
     };
+    // this should probably be maintained somehow
+    const GS2MLRelease =
+        "https://github.com/OmegaMetor/GS2ML/releases/download/9de51eb/gm2ml-win64.zip";
 
     // ICONS
     import GS2MLIcon from "$lib/components/SaveSlot/gs2ml.png";
@@ -67,87 +73,6 @@
     };
 
     // GS2ML
-    const installGs2ml = async () => {
-        // IMPORTANT: confirm & other functions are Promises in Tauri Svelte!
-
-        const message =
-            "Do you want to install GS2ML in the current game directory? This will disable GMML if that is installed.";
-        if (!(await confirm(message))) return;
-        // check for DOTNET 6.0
-        const dotnetSdkPaths = [
-            "C:/Program Files/dotnet/sdk/6.0.413",
-            "C:/Program Files/dotnet/sdk/6.0.316",
-            "C:/Program Files/dotnet/sdk/6.0.121",
-        ];
-        let exists = false;
-        for (const path of dotnetSdkPaths) {
-            const netExists = await fs.exists(path);
-            if (netExists) {
-                exists = true;
-                break;
-            }
-        }
-        if (!exists) {
-            // open installer
-            const message =
-                "DOTNET 6.0 does not seem to be installed. Download and open the installer now? At least 1 GB of space is recommended.";
-            if (await confirm(message)) {
-                console.log("checking system arch...");
-                const arch = await os.arch();
-                console.log("arch", arch);
-                let installation = NetInstallations.x64;
-                switch (arch) {
-                    // literally everything else installs x64 for now
-                    case "x86":
-                        // install x86
-                        installation = NetInstallations.x86;
-                        break;
-                    case "arm":
-                        // install arm
-                        installation = NetInstallations.arm;
-                        break;
-                }
-                // download the installer
-                console.log("downloading installer for this arch platform...");
-                fetch(installation).then((res) => {
-                    if (!res.ok) {
-                        console.warn("couldnt get installer!");
-                        return alert(
-                            "DOTNET 6.0 Installer could not be downloaded."
-                        );
-                    }
-                    res.arrayBuffer()
-                        .then((arraybuffer) => {
-                            fs.writeBinaryFile(
-                                "./dotnet_installer.exe",
-                                arraybuffer
-                            )
-                                .then(() => {
-                                    // TODO: run installer
-                                })
-                                .catch((err) => {
-                                    console.warn(
-                                        "couldnt write installer exe!",
-                                        err
-                                    );
-                                    return alert(
-                                        "DOTNET 6.0 Installer could not be downloaded."
-                                    );
-                                });
-                        })
-                        .catch((err) => {
-                            console.warn(
-                                "couldnt get installer as buffer!",
-                                err
-                            );
-                            return alert(
-                                "DOTNET 6.0 Installer could not be downloaded."
-                            );
-                        });
-                });
-            }
-        }
-    };
     const openGs2mlMenu = async () => {
         modalStates.gs2ml.open = true;
         modalStates.gs2ml.available = false;
@@ -186,6 +111,119 @@
         modalStates.gs2ml.available = true;
         modalStates.gs2ml.searching = false;
         modalStates.gs2ml.searchable = true;
+    };
+    const installGs2ml = async () => {
+        // IMPORTANT: confirm & other functions are Promises in Tauri Svelte!
+
+        const message =
+            "Do you want to install GS2ML in the current game directory? This will disable GMML if that is installed.";
+        if (!(await confirm(message))) return;
+        // check for DOTNET 6.0
+        const dotnetSdkPaths = [
+            "C:/Program Files/dotnet/sdk/6.0.413",
+            "C:/Program Files/dotnet/sdk/6.0.316",
+            "C:/Program Files/dotnet/sdk/6.0.121",
+        ];
+        let exists = false;
+        for (const path of dotnetSdkPaths) {
+            const netExists = await fs.exists(path);
+            if (netExists) {
+                exists = true;
+                break;
+            }
+        }
+        // if dotnet is not installed
+        if (!exists) {
+            // open installer
+            const message =
+                "DOTNET 6.0 does not seem to be installed. Download the installer now? You'll need to open it yourself and at least 1 GB of space is recommended.";
+            if (await confirm(message)) {
+                console.log("checking system arch...");
+                const arch = await os.arch();
+                console.log("arch", arch);
+                let usingArch = "x64";
+                let installation = NetInstallations.x64;
+                switch (arch) {
+                    // literally everything else installs x64 for now
+                    case "x86":
+                        // install x86
+                        installation = NetInstallations.x86;
+                        break;
+                    case "arm":
+                        // install arm
+                        installation = NetInstallations.arm;
+                        break;
+                }
+                if (installation !== NetInstallations.x64) {
+                    usingArch = arch;
+                }
+                // download the installer
+                console.log("downloading installer for", usingArch);
+                shell.open(installation); // open the webpage
+            }
+        }
+
+        // either while thats happening or just after nothing happened before,
+        // lets install GS2ML
+        console.log("fetching GS2ML archive");
+        http.fetch(GS2MLRelease, {
+            method: "GET",
+            responseType: http.ResponseType.Binary,
+        })
+            .then(async (res) => {
+                if (!res.ok) {
+                    console.error("GS2ML response NOT ok;", res.data);
+                    alert("Failed to download GS2ML.");
+                    return;
+                }
+                console.log("writing file");
+                const archivePath = await path.resolve(
+                    "./gs2ml_archive_snailtool.zip"
+                );
+                const archiveTarget = await path.join(FilePaths.game, "/gs2ml");
+                console.log("writing file to", archivePath);
+                fs.writeBinaryFile(archivePath, res.data)
+                    .then(async () => {
+                        const cmdCommand = `powershell -command "Expand-Archive "${archivePath}" "${archiveTarget}" -Force"`;
+                        console.log("running", cmdCommand);
+                        const commandResult = await new shell.Command(
+                            "extract-gs2ml",
+                            cmdCommand
+                        ).execute();
+                        console.log(commandResult.stdout);
+                        if (commandResult.code !== 0) {
+                            // epic fail
+                            // svelte formatting makes this look disgusting holy shit
+                            console.error(
+                                "command failed with code",
+                                commandResult.code,
+                                ";",
+                                commandResult
+                            );
+                            alert(
+                                "Failed to unzip GS2ML archive. Process failed with exit code " +
+                                    commandResult.code +
+                                    "."
+                            );
+                            return;
+                        }
+                        // we successed, reload
+                        modalStates.gs2ml.open = false;
+                        openGs2mlMenu();
+                    })
+                    .catch((err) => {
+                        console.error("couldnt write GS2ML zip;", err);
+                        alert(
+                            "Failed to write the GS2ML zip archive. You may not have enough disk space, or something else went wrong."
+                        );
+                        return;
+                    });
+            })
+            .catch((err) => {
+                console.error("couldnt fetch GS2ML;", err);
+                alert("Failed to download GS2ML.");
+                return;
+            });
     };
 </script>
 
